98bbab7b324ed3066e0f3665861acb71de6c4305
plummyyummy <sweetplumquaver@gmail.com>
2026-04-09 18:59:51 -0700

    update the shit

--- plugin.lua
+++
@@ -1,9 +0,0 @@
-math.randomseed(os.time())
-imgui_disable_vector_packing=true
-PLUGIN_NAME="LuaverTest";PLUGIN_VERSION="1.0.0";PLUGIN_AUTHOR="plummyyummy";PLUGIN_DESCRIPTION="Nothing Special"
-function awake()
-print("awake")
-end
-function draw()
-print("draw")
-end
