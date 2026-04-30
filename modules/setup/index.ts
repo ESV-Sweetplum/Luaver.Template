f3f189e7f4d5e0bd835e3e7e58a6494771f10891
plummyyummy <sweetplumquaver@gmail.com>
2026-04-29 18:49:12 -0700

    move types to devdep, start setup process

--- modules/setup/index.ts
+++ modules/setup/index.ts
@@ -1,1 +1,21 @@
-console.log('hi');
+import * as inquirer from '@inquirer/prompts';
+import LuaverConfig from '../../Luaver/src/interfaces/luaverConfig';
+import chalk from 'chalk';
+import * as json5 from 'json5';
+import * as fs from 'fs';
+
+export default async function main() {
+    let existingConfig: Partial<LuaverConfig> = {};
+    if (fs.existsSync('luaverConfig.json5')) {
+        existingConfig = json5.parse(fs.readFileSync('luaverConfig.json5', 'utf-8'));
+    }
+
+    const pluginName = await inquirer.input({
+        message: 'Please enter a name for your plugin.',
+        default: existingConfig.pluginName,
+        transformer: (i: string) => chalk.magenta(i),
+    });
+}
+try {
+    main();
+} catch (e) {}
